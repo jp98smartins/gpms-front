edaa2e5b0cbc6a4a839ba383980006d90bca4143
Ricardo Carletti <ricardocarletti@id.uff.br>
2022-05-15 21:27:28 -0300

    [FEAT] Finalizando Splash Page com Rota

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,81 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-import '../../core/theme/app_assets.dart';
-import '../../core/theme/app_colors.dart';
-import '../../core/theme/app_fonts.dart';
 import '../../core/adapters/svg_image_adapter.dart';
+import '../../core/routes/app_routes.dart';
+import '../../core/theme/app_assets.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // não consegui usar a merda do tema, trocar quando souber
-    const headline1 = TextStyle(
-      color: AppColors.foregroundPrimary,
-      fontFamily: AppFonts.title,
-      fontSize: 96.0,
-      fontWeight: FontWeight.w700,
+    Future.delayed(
+      const Duration(milliseconds: 1500),
+      () => Modular.to.popAndPushNamed(AppRoutes.menuRoute),
     );
-
-    const button = TextStyle(
-      color: AppColors.backgroundSecondary,
-      fontFamily: AppFonts.primary,
-      fontSize: 20.0,
-      fontWeight: FontWeight.bold,
-    );
-
-    var itemsGameMode = <String>['PvP', 'PvE', 'EvE']
-        .map<DropdownMenuItem<String>>((String value) {
-      return DropdownMenuItem<String>(
-        value: value,
-        child: Text(value),
-      );
-    }).toList();
-    var valueGameMode = 'PvP';
-
-    var itemsGameLevel = <String>['Fácil', 'Médio', 'Difícil']
-        .map<DropdownMenuItem<String>>((String value) {
-      return DropdownMenuItem<String>(value: value, child: Text(value));
-    }).toList();
-    var valueGameLevel = 'Fácil';
-
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgImageAdapter.fromAsset(
-              AppAssets.whiteIcon,
-              key: const Key("AppIcon"),
-              width: 175.0,
-              semantics: "Icone do Aplicativo",
-            ),
-            const Text(
-              'xadrez',
-              style: headline1,
-            ),
-            const SizedBox(height: 10),
-            DropdownButton(
-              value: valueGameMode,
-              items: itemsGameMode,
-              onChanged: (String? newValue) {},
-            ),
-            const SizedBox(height: 10),
-            DropdownButton(
-              value: valueGameLevel,
-              items: itemsGameLevel,
-              onChanged: (String? newValue) {},
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text(
-                "Iniciar Jogo",
-                style: button,
-              ),
-            )
-          ],
+        child: SvgImageAdapter.fromAsset(
+          AppAssets.whiteIcon,
+          key: const Key("AppIcon"),
+          width: 175.0,
+          semantics: "Icone do Aplicativo",
         ),
       ),
     );
