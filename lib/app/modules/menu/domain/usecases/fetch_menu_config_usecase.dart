7178793c0bd8f46522a1025000650b960f0ddc3e
Rafael Lins Pinheiro <rafaellinsp@gmail.com>
2022-05-15 23:57:00 -0300

    [FIX] Refatorando usecases

--- lib/app/modules/menu/domain/usecases/fetch_menu_config_usecase.dart
+++ lib/app/modules/menu/domain/usecases/fetch_menu_config_usecase.dart
@@ -1,12 +1,12 @@
 import '../../data/dtos/menu_config_dto.dart';
 
 abstract class IFetchMenuConfigUseCase {
-  Future<MenuConfigDto> call();
+  Future<MenuConfigDto?> call();
 }
 
 class FetchMenuConfigUseCase implements IFetchMenuConfigUseCase {
   @override
-  Future<MenuConfigDto> call() async {
+  Future<MenuConfigDto?> call() async {
     throw UnimplementedError();
   }
 }
