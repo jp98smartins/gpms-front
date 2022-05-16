98f24471861a38f0af8fe83329c2b5e3e49226ca
Rafael Lins Pinheiro <rafaellinsp@gmail.com>
2022-05-15 23:16:59 -0300

    [FEAT] Criando usecases menu config

---
+++ lib/app/modules/menu/domain/usecases/save_menu_config_usecase.dart
@@ -0,0 +1,10 @@
+import '../../data/dtos/menu_config_dto.dart';
+
+abstract class ISaveMenuConfigUseCase {
+  Future<bool> call(MenuConfigDto menuConfigDto);
+}
+
+class SaveMenuConfigUseCase implements ISaveMenuConfigUseCase {
+  @override
+  Future<bool> call(MenuConfigDto menuConfigDto) async {}
+}
