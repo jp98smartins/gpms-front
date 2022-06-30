0327f929760013f02fd2f97e160ef9e96c8fd78f
João Pedro Martins <joaopedromartins@id.uff.br>
2022-06-30 17:41:19 -0300

    travando modo vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/services.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(
-      ModularApp(
-        module: AppModule(),
-        child: const AppWidget(),
-      ),
-    );
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
+}
